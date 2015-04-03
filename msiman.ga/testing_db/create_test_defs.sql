f02f80fcf0ef5a5d47ad916e4713969aa7555bcd
Mpumelelo Msimanga <mmsimanga@gmail.com>
2015-04-03 14:59:48 +0200

    DDL files for testing set of tables

---
+++ msiman.ga/testing_db/create_test_defs.sql
@@ -0,0 +1,9 @@
+CREATE TABLE
+        (
+                test_id INT PRIMARY KEY
+                ,is_current BOOLEAN
+                ,test_name VARCHAR(50)
+                ,test_source VARCHAR(50)
+                ,test_target VARCHAR(50)
+                ,test_sql VARCHAR(2000)
+        )
---
+++ msiman.ga/testing_db/create_test_logs.sql
@@ -0,0 +1,7 @@
+REATE TABLE test_logs
+        (
+                test_id INT PRIMARY KEY
+                ,test_date DATE
+                ,test_db VARCHAR(50)
+                ,test_result NUMERIC(10,2)
+        )
